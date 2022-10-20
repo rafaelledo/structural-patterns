81248589d1052a2e7e0bf88167a58c777c6b37f3
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-10-20 08:21:30 -0300

    Refactor

--- src/main/java/br/com/adapter/hexagonal/app/UserRest.java
+++ src/main/java/br/com/adapter/hexagonal/app/UserRest.java
@@ -10,27 +10,29 @@ import br.com.adapter.hexagonal.core.model.User;
 import br.com.adapter.hexagonal.core.ports.UserRepository;
 
 public class UserRest {
-	private UserService userFacade;
+	private UserService userService;
 
 	public UserRest() {
 		UserRepository userRepo = new UserMemoryDatabaseAdapter();
-		userFacade = new UserService(userRepo);
+		userService = new UserService(userRepo);
 	}
 
 	public Integer post(Map<String, String> values) {
 		try {
 			User user = new User(values.get("name"), values.get("email"), values.get("password"));
-			userFacade.saveUser(user);
+			userService.saveUser(user);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			return 400;
-		}
+		}	
 		return 201;
 	}
 
 	public Integer get() {
-		List<User> users = userFacade.getUsers();
-		users.stream().forEach(System.out::println);
+		List<User> users = userService.getUsers();
+		users.stream().forEach(user -> {
+			System.out.println(user);
+		});
 		return 200;
 	}
 }
